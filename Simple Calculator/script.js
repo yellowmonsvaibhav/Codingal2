2d651cfbdd3cd36bf3e323833b3d9ee9d7f1945a
@CrazyVaibhav20 <155980475+yellowmonsvaibhav@users.noreply.github.com>
2025-11-11 17:31:57 +0530

    Add files via upload

---
+++ Simple Calculator/script.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display');
+
+function appendValue(value) {
+  display.value += value;
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = 'Error';
+  }
+}
+
+console.log(display);
+console.log(appendValue);
+console.log(clearDisplay);
+console.log(deleteLast);
+console.log(calculate);
