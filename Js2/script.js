656cbfbc45b2bb6df97a2afeaad42fb64d880dc5
@CrazyVaibhav20 <155980475+yellowmonsvaibhav@users.noreply.github.com>
2025-11-19 16:22:48 +0530

    Add random number generation and string methods

---
+++ Js2/script.js
@@ -0,0 +1,33 @@
+
+console.log("Math.random():", Math.random());
+console.log("Another random:", Math.random());
+
+
+let random1to10 = Math.floor(Math.random() * 10) + 1;
+console.log("Random 1-10:", random1to10);
+
+
+console.log(Math.floor(3.99));
+console.log(Math.floor(56.8));
+
+
+console.log("Math.ceil(3.01):", Math.ceil(3.91));
+
+
+console.log("Math.round(8.4):", Math.round(8.4));
+console.log("Math.round(4.6):", Math.round(9.6));
+
+console.log(2**3)
+
+
+{const message= "I like HTML,CSS,JavaScript and want to learn React, Python ,C andC++"
+
+console.log("Ends with 'Hello':", message.endsWith("Hello"));
+console.log("Includes  'Java':", message.startsWith("Java"));
+
+
+console.log("Includes 'Script':", message.includes("Script"));
+console.log("Includes 'Python':", message.includes("Python"));
+
+
+console.log("Length of string:", message.length);}
