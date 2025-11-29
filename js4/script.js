9cb34364c7fe34637f5c961cda431e3ac632dbdc
@CrazyVaibhav20 <155980475+yellowmonsvaibhav@users.noreply.github.com>
2025-11-29 16:58:11 +0530

    Add script.js to display fruit names with emojis

---
+++ js4/script.js
@@ -0,0 +1,13 @@
+
+    let fruits = ["Apple", "Banana", "Orange" ,"Watermelon", "Grapes"];
+    let result1 = fruits.join(" 😋🥰!🥰😋 ")
+    console.log(result1);
+     alert(result1)
+
+    let fruits2 = ["Apple", "Banana", "Orange" ,"Watermelon", "Grapes"];
+    let result2 = fruits.join("  😋Ta🥰sty😋  ")
+    console.log(result2);
+    alert(result2)
+
+
+       
